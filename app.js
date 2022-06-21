962da63be3323fe796104937906c6b07ba05bfa2
LuisEUM <luiseum95@gmail.com>
2022-06-21 21:35:53 +0200

    Ahora funciona el CSS

--- app.js
+++ app.js
@@ -4,16 +4,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// hace que podamos saltar el nombre del directorio public y enlazar directamente el css sin pasar por esta carpeta
+app.use(express.static(`${__dirname}/public`))
+app.use(express.urlencoded())
+
 // req= request
 // res respond
-app.get('/',(req, resp, next)=>{
+app.get('/',(req, res, next)=>{
     // resp.send('Home') escribir directamente en elhtml 
-    resp.sendFile(`${__dirname}/views/index.html`)
+    res.sendFile(`${__dirname}/views/index.html`)
 })
 
-app.get('/about',(req, resp, next)=>{
+app.get('/about',(req, res, next)=>{
     // resp.send('About') escribir directamente en el html
-    resp.sendFile(`${__dirname}/views/about.html`)
+    res.sendFile(`${__dirname}/views/about.html`)
 })
+
+app.get("/posts/:id", (req, res, next)=>{
+    console.log(req.params)
+    const id = req.params.id
+    res.send(`welcome to post route: ${id}`)
+})
+
+app.get("/search", (req, res, next)=>{
+    console.log(req.query)
+    const id = req.query.id
+    res.send(`welcome to post route: ${id}`)
+})
+
 
 app.listen(port, () => console.log(`This is working at the port: ${port}`))
